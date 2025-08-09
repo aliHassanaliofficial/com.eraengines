e72c9fdd959a155acc0c7ae84e7f99be8ce3013b
aliHassanaliofficial <alihassanmagix2017@gmail.com>
2025-08-09 19:30:18 +0300

    new file:   .next/app-build-manifest.json new file:   .next/build-manifest.json new file:   .next/cache/.rscinfo new file:   .next/cache/webpack/client-development/0.pack.gz new file:   .next/cache/webpack/client-development/index.pack.gz new file:   .next/cache/webpack/server-development/0.pack.gz new file:   .next/cache/webpack/server-development/index.pack.gz new file:   .next/package.json new file:   .next/prerender-manifest.json new file:   .next/react-loadable-manifest.json new file:   .next/routes-manifest.json new file:   .next/server/app-paths-manifest.json new file:   .next/server/app/_not-found/page.js new file:   .next/server/app/_not-found/page_client-reference-manifest.js new file:   .next/server/interception-route-rewrite-manifest.js new file:   .next/server/middleware-build-manifest.js new file:   .next/server/middleware-manifest.json new file:   .next/server/middleware-react-loadable-manifest.js new file:   .next/server/next-font-manifest.js new file:   .next/server/next-font-manifest.json new file:   .next/server/pages-manifest.json new file:   .next/server/server-reference-manifest.js new file:   .next/server/server-reference-manifest.json new file:   .next/server/vendor-chunks/@swc.js new file:   .next/server/vendor-chunks/next.js new file:   .next/server/webpack-runtime.js new file:   .next/static/chunks/_app-pages-browser_node_modules_next_dist_client_dev_noop-turbopack-hmr_js.js new file:   .next/static/chunks/app-pages-internals.js new file:   .next/static/chunks/app/_not-found/page.js new file:   .next/static/chunks/main-app.js new file:   .next/static/chunks/polyfills.js new file:   .next/static/chunks/webpack.js new file:   .next/static/development/_buildManifest.js new file:   .next/static/development/_ssgManifest.js new file:   .next/static/webpack/633457081244afec._.hot-update.json new file:   .next/static/webpack/9708989783daa426.webpack.hot-update.json new file:   .next/static/webpack/webpack.9708989783daa426.hot-update.js new file:   .next/trace new file:   .next/types/cache-life.d.ts new file:   .next/types/package.json modified:   api/index.ts modified:   app.js modified:   package-lock.json modified:   package.json new file:   public/style/globals.css modified:   vercel.json modified:   views/edu_teachers.html
    anges not staged for commit:
    modified:   .gitignore
    
    tracked files:
    .vercel/

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,2 @@
-const app = require('../app'); // <-- Import from root ./app.js
-
-// Optional: only listen if you want to override port (usually not needed if app.js does that)
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server ready on port ${port}`);
-});
+const app = require('../app');
+module.exports = app;
--- app.js
+++ app.js
@@ -3,21 +3,26 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-
-const rootPath = path.resolve(__dirname, '..'); // 🔑 Fix path for Vercel
+const port = process.env.PORT || 3000;
+const rootPath = process.cwd();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(rootPath, 'public')));
+app.use(express.static(path.join(rootPath, 'public', 'style')));
 app.use(express.static(path.join(rootPath, 'app')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(rootPath, 'views', 'index.html'));
+  res.sendFile(path.join(rootPath, 'views', 'index.html'));
 });
 
 app.get('/websites/edu/teachers', (req, res) => {
-    res.sendFile(path.join(rootPath, 'views', 'edu_teachers.html'));
+  res.sendFile(path.join(rootPath, 'views', 'edu_teachers.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
